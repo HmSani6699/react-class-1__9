fe3f083c50b234ebfc9fa8ddca09a92d71897f43
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-21 02:10:44 +0600

    show the country title and flag

--- js/country.js
+++ js/country.js
@@ -1,6 +1,22 @@
 const loadCountry = () => {
-    fetch('https://restcountries.com/v3.1/all')
+    fetch('http://127.0.0.1:5500/js/data.json')
         .then(res => res.json())
         .then(data => displayData(data))
 }
 
+const displayData = (countries) => {
+    console.log(countries[0])
+    const container = document.getElementById('container');
+    const countryHtml = countries.map(country => getSingleCountry(country)).join(' ');
+    container.innerHTML = countryHtml
+}
+
+const getSingleCountry = (country) => {
+    return `
+            <div>
+            <h1>${country.name.common}</h1>
+            <img src="${country.flags.png}" alt="">
+            </div>
+            `
+}
+loadCountry()
