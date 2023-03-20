80626cbf5b915f859c818117daedacd3db1a141a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-20 23:43:43 +0600

    load country data complete

--- js/country.js
+++ js/country.js
@@ -1,1 +1,6 @@
-console.log('alhamdolillah valo')
+const loadCountry = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayData(data))
+}
+
